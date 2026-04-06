f5b589944d6f685e7487beb97b721ea9463dc69d
BQFIRE <vpodzhigaylo@mail.ru>
2026-04-06 21:42:28 +0300

    добавлены юнит тесты

---
+++ p4pr/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0146de7-e1ba-4d08-8710-c0d3505cc71d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ p4pr/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p4pr;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Page1()
+        {
+            var page = new Page1();
+
+            Assert.IsNotNull(page);
+        }
+
+        [TestMethod]
+        public void Test_Page2()
+        {
+            var page = new Page2();
+
+            Assert.IsNotNull(page);
+        }
+
+        [TestMethod]
+        public void Test_Page3()
+        {
+            var page = new Page3();
+
+            Assert.IsNotNull(page);
+        }
+    }
+}
